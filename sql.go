c6d1b0a2583140ba062b568fed63c50ae972c059
YongbinKim <iam@yongbin.kim>
2019-07-08 05:14:53 +0900

    Memory bug fix

--- sql.go
+++ sql.go
@@ -25,7 +25,12 @@ func (id *ID) Scan(src interface{}) error {
 		return errors.New("ids: unsupported type")
 	}
 
-	newID := (*ID)(&data)
+	if len(data) != 8 {
+		return errors.New("ids: invalid id format")
+	}
+
+	// Create new ID
+	newID := &ID{data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]}
 	if err := newID.Valid(); err != nil {
 		return fmt.Errorf("ids: %s", err)
 	}
